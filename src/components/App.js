9c8f48c9c858d8c2a2bd3f5bb32f3c7e865b8e00
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-07 20:31:55 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,25 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
+  const startGame = () => {
+    setRenderBall(true);
+  };
   const reset = () => {};
-  const renderChoice = () => {};
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball"></div>;
+    } else {
+      return (
+        <button className="start" onClick={startGame}>
+          Start
+        </button>
+      );
+    }
+  };
 
   return (
     <div className="playground">
