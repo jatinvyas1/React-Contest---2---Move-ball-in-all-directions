dbd8912602e85b357ac39a68d27090b1a019a06f
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-07 22:00:37 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -13,10 +13,15 @@ const App = () => {
     setRenderBall(true);
   };
 
-  const reset = () => {};
+  function reset() {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({ left: "0px", top: "0px" });
+  }
   const renderChoice = () => {
     if (renderBall) {
-      return <div className="ball"></div>;
+      return <div className="ball" style={ballPosition}></div>;
     } else {
       return (
         <button className="start" onClick={startGame}>
@@ -25,27 +30,34 @@ const App = () => {
       );
     }
   };
-  const handleKeyPress = useCallback(
-    (event) => {
-      if (event.keyCode === 37) {
-        setX(x - 5);
-      } else if (event.keyCode === 38) {
-        setY(y + 5);
-      } else if (event.keyCode === 39) {
-        setX(x + 5);
-      } else if (event.keyCode === 40) {
-        setY(y - 5);
-      }
-      setBallPosition({ left: x + "px", top: y + "px" });
-      console.log("clicked");
-    },
-    [x, y]
-  );
+  const handleKeyPress = (event) => {
+    if (event.keyCode === 37) {
+      setX(x - 5);
+    } else if (event.keyCode === 38) {
+      setY(y - 5);
+    } else if (event.keyCode === 39) {
+      setX(x + 5);
+    } else if (event.keyCode === 40) {
+      setY(y + 5);
+    }
+  };
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
     return () => window.removeEventListener("keydown", handleKeyPress);
-  }, [handleKeyPress]);
+  });
+  useEffect(() => {
+    setBallPosition({
+      left: x + "px",
+      top: ballPosition.top
+    });
+  }, [x]);
+  useEffect(() => {
+    setBallPosition({
+      left: ballPosition.left,
+      top: y + "px"
+    });
+  }, [y]);
 
   return (
     <div className="playground">
